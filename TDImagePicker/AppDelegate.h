605f9247c8113903be7c2048fc92e84b8c047191
xiaodian <sjd@sudeimac.local>
2015-11-04 15:28:39 +0800

    Initial Commit

---
+++ TDImagePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TDImagePicker
+//
+//  Created by Su Jiandong on 15/11/4.
+//  Copyright © 2015年 Su Jiandong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TDImagePickerTests/TDImagePickerTests.m
@@ -0,0 +1,39 @@
+//
+//  TDImagePickerTests.m
+//  TDImagePickerTests
+//
+//  Created by Su Jiandong on 15/11/4.
+//  Copyright © 2015年 Su Jiandong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TDImagePickerTests : XCTestCase
+
+@end
+
+@implementation TDImagePickerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
